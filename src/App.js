f3773a1bfe456c0e6481fa4e98fd92175500124f
camilamejiax <96745732+camilamejiax@users.noreply.github.com>
2022-04-11 12:44:52 -0400

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App 
+    <h1>Weather App </h1>
+    <footer> This project was coded by Camila Mejia and is 
+      <a href="https://github.com/camilamejiax/react-weather-app2" target="_blank"> open-sourced on GitHub</a>
+    </footer>
     </div>
   );
 }
