78b2220510f08dabfd1c8675db8968958289ee99
camilamejiax <96745732+camilamejiax@users.noreply.github.com>
2022-04-11 12:54:19 -0400

    added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Weather App </h1>
-    <footer> This project was coded by Camila Mejia and is 
-      <a href="https://github.com/camilamejiax/react-weather-app2" target="_blank"> open-sourced on GitHub</a>
-    </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          {" "}
+          This project was coded by Camila Mejia and is
+          <a
+            href="https://github.com/camilamejiax/react-weather-app2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
