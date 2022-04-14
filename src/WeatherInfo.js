3469a83d1069b99aa3ab1a6fd736b3654b315d8e
camilamejiax <96745732+camilamejiax@users.noreply.github.com>
2022-04-14 14:48:15 -0400

    Added AnimatedWeather, updated CSS for the logo, and matched the icon number with its name

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -16,11 +17,9 @@ export default function WeatherInfo(props) {
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
-              <img
-                src={props.data.imgIcon}
-                alt={props.data.description}
-                className="float-left"
-              />
+              <div className="flot-left">
+              <WeatherIcon code={props.data.icon} />
+              </div>
 
               <div className="float-left">
                 <span className="temperature">
